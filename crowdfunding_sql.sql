76b00c0e2484729cf522118e437db0407f1985a8
simpelaki <117967087+simpelaki@users.noreply.github.com>
2023-03-10 18:44:31 -0600

    Add files via upload

---
+++ crowdfunding_sql.sql
@@ -0,0 +1,61 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/Oe5VH5
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "Campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(255)   NOT NULL,
+    "description" VARCHAR(255)   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "outcome" VARCHAR(255)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(255)   NOT NULL,
+    "currency" VARCHAR(255)   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(255)   NOT NULL,
+    "subcategory_id" VARCHAR(255)   NOT NULL
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "email" VARCHAR(255)   NOT NULL
+);
+
+CREATE TABLE "Category" (
+    "category_id" VARCHAR(255)   NOT NULL,
+    "category" VARCHAR(255)   NOT NULL
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" VARCHAR(255)   NOT NULL,
+    "subcategory" VARCHAR(255)   NOT NULL
+);
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_company_name" FOREIGN KEY("company_name")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
+SELECT*
+FROM "Campaign"
+
+SELECT*
+FROM "Category"
+
+SELECT*
+FROM "Subcategory"
+
+SELECT*
+FROM "Contacts"
